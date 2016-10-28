1e550b060d43ddadfc3a92432fb7d494dd0786e3
Cumar221 <omar221@windowslive.com>
2016-10-27 19:15:56 -0600

    added unit testing classes

---
+++ CPSC 501 - Assignment 2/src/TestJunit.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import java.io.FileNotFoundException;
+
+
+public class TestJunit {
+	
+   @Test
+   public void testGetTeamStat() throws FileNotFoundException  {
+	 
+  }
+}
